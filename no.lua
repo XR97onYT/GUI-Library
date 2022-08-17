2b1942efccd2cbdb5b996483aa2efaeb2fbfaeb7
XR97onYT <83871996+XR97onYT@users.noreply.github.com>
2022-08-16 19:50:52 -0500

    pingspoof

---
+++ no.lua
@@ -0,0 +1,25 @@
+local RandomLag = math.random(4,8)
+
+local MT = getrawmetatable(game)
+local OriginalNamecall = MT.__namecall
+
+setreadonly(MT, false)
+
+MT.__namecall = newcclosure(function(self, ...)
+	local Arguments = {...}
+	local NCM = getnamecallmethod()
+
+	if tostring(NCM) == "FindPartOnRayWithIgnoreList" then
+		table.insert(Arguments[2], workspace.Map)
+	end
+	if tostring(NCM) == "FireServer" then
+		if tostring(self) == "UpdatePing" then
+			Arguments[1] = RandomLag
+			RandomLag = math.random(4,8)
+			return OriginalNamecall(self, unpack(Arguments))
+		end
+	end
+	return OriginalNamecall(self, ...)
+end)
+
+setreadonly(MT, true)
